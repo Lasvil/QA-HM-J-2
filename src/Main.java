4818e7d2e4af2a11f8efe86fdee900ac748999fe
ArtyomKiyashko <lounershoargent@gmail.com>
2023-05-31 20:02:55 +0300

    Finish coding

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 10000;
+        int bonus = 20;
+
+        int bMiles = price/bonus;
+
+        System.out.print(bMiles);System.out.println(" Бонусных миль");
+    }
+}
