8c7f3519af4e9e299b3e4423af513b81d2a6def9
ArtyomKiyashko <lounershoargent@gmail.com>
2023-06-01 18:50:57 +0300

    Change Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        int price = 10000;
+        int price = 36987;
         int bonus = 20;
 
-        int bMiles = price/bonus;
+        int bMiles = price / bonus;
 
-        System.out.print(bMiles);System.out.println(" Бонусных миль");
+        System.out.print(bMiles);
+        System.out.println(" Бонусных миль");
     }
 }
